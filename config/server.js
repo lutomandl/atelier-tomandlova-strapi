5436eb7023240ec93014b3e8822d29530624309b
Lubos Tomandl <lutomandl@gmail.com>
2021-06-01 18:43:49 +0000

    Initializing Strapi repozitory

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5166f40a88c721f37db32588a0f9b468'),
+    },
+  },
+});
