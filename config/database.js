9f5f8f89c9872b3119afc46b2f9676bd65f9954e
Lubos Tomandl <lutomandl@gmail.com>
2021-06-05 09:34:59 +0000

    add posts content type

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'atelier-tomandlova-strapi'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        username: env('DATABASE_USERNAME', 'AdminLubos'),
+        password: env('DATABASE_PASSWORD', 'wlakdbf2'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
